68a2e809bfb3e4ba678ab447d5e275c209e033b2
ghost <kirill.han011@gmail.com>
2022-11-06 21:12:02 +0300

    реализовано добавление нескольких картинок

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import shutil
+from typing import List
 from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
@@ -6,7 +7,17 @@ app = FastAPI()
 
 @app.post('/')
 async def root(file: UploadFile = File(...)):
-    with open('test.mp4', "wb") as buffer:
+    with open(f'{file.filename}', "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
     return {"file_name": file.filename}
+
+
+@app.post('/img')
+async def upload_image(files: List[UploadFile] = File(...)):
+    for img in files:
+        with open(f'{img.filename}', "wb") as buffer:
+            shutil.copyfileobj(img.file, buffer)
+
+    return {"file_name": "Good"}
+
