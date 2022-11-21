0c1df549ceb935250b5aeef0384930ed5f3932f9
ghost <kirill.han011@gmail.com>
2022-11-21 14:18:39 +0300

    добавлено создание видео через модель Video. так же удалил тестовую функцию по загрузке картинок. функция root переименована на функцию create_video

--- api.py
+++ api.py
@@ -1,37 +1,21 @@
 import shutil
-from typing import List
 from fastapi import APIRouter, UploadFile, File, Form
 
 from schemas import UploadVideo, GetVideo, Message
-from models import Video
+from models import Video, User
 
 video_router = APIRouter()
 
 
 @video_router.post('/')
-async def root(title: str = Form(...), description: str = Form(...), file: UploadFile = File(...)):
+async def create_video(title: str = Form(...), description: str = Form(...), file: UploadFile = File(...)):
     info = UploadVideo(title=title, description=description)
     with open(f'{file.filename}', "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
-
-    return {"file_name": file.filename, "info": info}
-
-
-@video_router.post('/img')
-async def upload_image(files: List[UploadFile] = File(...)):
-    for img in files:
-        with open(f'{img.filename}', "wb") as buffer:
-            shutil.copyfileobj(img.file, buffer)
-
-    return {"file_name": "Good"}
+    user = await User.objects.first()
+    return await Video.objects.create(user=user, **info.dict())
 
 
-@video_router.post("/video")
-async def create_video(video: Video):
-    await video.save()
-    return video
-
-
-@video_router.get('/video/{video_pk}', response_model=Video, responses={404: {"model": Message}})
+@video_router.get('/video/{video_pk}', response_model=GetVideo, responses={404: {"model": Message}})
 async def get_video(video_pk: int):
     return await Video.objects.select_related("user").get(pk=video_pk)
